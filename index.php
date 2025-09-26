54fe76bc7c03e4ceeb37f3b6546cf3858e602366
Mateus Motta <56051761+Motta21@users.noreply.github.com>
2025-09-26 09:21:17 -0300

    feat: Subindo o HTML base

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <title>CircuitByte</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+
+  <nav class="navbar">
+        <ul>
+            <li><a href="#Home">Início</a></li>
+            <li><a href="#Produtos">Sobre</a></li>
+            <li><a href="#Venda">Serviços</a></li>
+            <li><a href="#Historico de Vendas">Contato</a></li>
+            <li><a href="#Relatório de vendas">Contato</a></li>
+        </ul>
+
+</body>
+</html>
