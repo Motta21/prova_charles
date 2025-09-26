8e5673922a091cc2923fd3af18b1a844aa6fd0fc
luizkikinho <luiz.caetano2@fatec.sp.gov.br>
2025-09-26 10:53:01 -0300

    feat: Adicionado as lógicas de busca da venda

--- app/models/Venda.php
+++ app/models/Venda.php
@@ -11,11 +11,35 @@
         private $conexao;
 
         public function __construct($pdo) {
-
+            $this->conexao = $pdo;
         }
 
         public function registrar($id_produto, $quantidade) {
-            
+
+        }
+
+        public function listarVendas() {
+            try {
+                $sql = "SELECT * FROM vendas";
+                $stmt = $this->conexao->prepare($sql);
+                $stmt->execute();
+                return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                error_log($e->getMessage());
+                die("Erro ao listar vendas: " . $e->getMessage());
+            }
+        }
+
+        public function buscarVendaId($id) {
+            try {
+                $sql = "SELECT * FROM vendas WHERE ID_VENDAS = ?";
+                $stmt = $this->conexao->prepare($sql);
+                $stmt->execute([$id_venda]);
+                return $stmt->fetch(PDO::FETCH_ASSOC);
+            } catch (PDOException $e) {
+                error_log($e->getMessage());
+                die("Erro ao buscar a venda no ID: " . $e->getMessage());
+            }
         }
     }
 ?>
